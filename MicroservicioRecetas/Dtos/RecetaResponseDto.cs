7cc6fb175d241804c1bf2a2e11b05d94e31e5699
Jorge Fonseca <65515144+camjor@users.noreply.github.com>
2025-03-03 15:47:17 -0500

    Delete MicroservicioRecetas directory

--- MicroservicioRecetas/Dtos/RecetaResponseDto.cs
+++
@@ -1,9 +0,0 @@
-public class RecetaResponseDto {
-        public int Id { get; set; }
-        public string? Codigo { get; set; } // Código único
-        public string? PacienteId { get; set; }
-
-        public string? MedicoId { get; set; }
-        public string? Descripcion { get; set; }
-        public string? Estado { get; set; } // "Activa", "Vencida", "Entregada"
-}
